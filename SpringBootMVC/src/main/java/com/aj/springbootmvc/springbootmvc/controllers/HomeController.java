5e34ccfa71bdd297e3adc731f17d649138b751e6
Aman Jolly <42204610+amanjolly@users.noreply.github.com>
2025-04-30 12:14:45 +0530

    language added

--- SpringBootMVC/src/main/java/com/aj/springbootmvc/springbootmvc/controllers/HomeController.java
+++ SpringBootMVC/src/main/java/com/aj/springbootmvc/springbootmvc/controllers/HomeController.java
@@ -1,26 +1,26 @@
 package com.aj.springbootmvc.springbootmvc.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home1() {
         System.out.println("Hello World MVC");
         return "index.jsp";
     }
 
-    @RequestMapping("/modelview")
+    @GetMapping("/modelview")
     public ModelAndView home() {
-
         return new ModelAndView("userinput.jsp");
     }
 
-    @RequestMapping("add")
+    @PostMapping("add")
     public ModelAndView add(@RequestParam(name = "firstNumber") int num1, @RequestParam(name =
             "secondNumber") int num2) {
         int num3 = num1 + num2;
---
+++ SpringBootMVC/web/WEB-INF/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="utf-8" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>SpringBOOT MVC</title>
+</head>
+<body>
+Welcome to SpringBoot MVC
+</body>
+</html>
